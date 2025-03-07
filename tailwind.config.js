2918ca17e7c77cacdc0d68b4cf7c397b17e2feb7
Burak Esenoğlu <burakesngluuu@gmail.com>
2025-03-07 16:00:37 +0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,11 +32,4 @@ export default {
     },
   },
   plugins: [],
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      './src/**/*.{js,jsx,ts,tsx}',
-      './public/index.html',
-    ],
-  }
 } 
